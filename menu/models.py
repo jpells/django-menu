d7829835807c8332324bb9bf97140c9891a21b08
James.Pells <James.Pells@0c545165-fa40-0410-a303-bf562d6d1485>
2007-12-19 23:32:54 +0000

    change __str__ to __unicode__ method

--- menu/models.py
+++ menu/models.py
@@ -1,41 +1,42 @@
-from django.db import models  
-   
-class Menu(models.Model):  
-    name = models.CharField(max_length=100)  
-    slug = models.SlugField()  
-    base_url = models.CharField(max_length=100, blank=True, null=True)  
-    description = models.TextField(blank=True, null=True)  
-   
-    class Admin:  
-        pass  
-   
-    def __unicode__(self):  
-        return "%s" % self.name  
-   
-    def save(self):  
-        """ 
-        Re-order all items at from 10 upwards, at intervals of 10. 
-        This makes it easy to insert new items in the middle of  
-        existing items without having to manually shuffle  
-        them all around. 
-        """  
-        super(Menu, self).save()  
-           
-        current = 10  
-        for item in MenuItem.objects.filter(menu=self).order_by('order'):  
-            item.order = current  
-            item.save()  
-            current += 10  
-  
-class MenuItem(models.Model):  
-    menu = models.ForeignKey(Menu, edit_inline=models.TABULAR, num_in_admin=3, num_extra_on_change=3)  
-    order = models.IntegerField(core=True)  
+from django.db import models
+from django.utils.translation import ugettext as _
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    base_url = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Admin:
+        pass
+ 
+    def __unicode__(self):
+        return _("%s" % self.name)
+ 
+    def save(self):
+        """
+        Re-order all items at from 10 upwards, at intervals of 10.
+        This makes it easy to insert new items in the middle of
+        existing items without having to manually shuffle
+        them all around.
+        """
+        super(Menu, self).save()
+ 
+        current = 10
+        for item in MenuItem.objects.filter(menu=self).order_by('order'):
+            item.order = current
+            item.save()
+            current += 10
+ 
+class MenuItem(models.Model):
+    menu = models.ForeignKey(Menu, edit_inline=models.TABULAR, num_in_admin=3, num_extra_on_change=3)
+    order = models.IntegerField(core=True)
     url_name = models.CharField(max_length=100, help_text='URL Name for Reverse Lookup, eg comments.comment_was_posted', blank=True, null=True, core=True)
     view_path = models.CharField(max_length=100, help_text='Python Path to View to Render, eg django.contrib.admin.views.main.index', blank=True, null=True, core=True)
     link_url = models.CharField(max_length=100, help_text='URL or URI to the content, eg /about/ or http://foo.com/', blank=True, null=True, core=True)
     title = models.CharField(max_length=100, core=True)
     login_required = models.BooleanField(blank=True, null=True, core=True)
     staff_required = models.BooleanField(blank=True, null=True, core=True)
-       
-    def __unicode__(self):  
-        return "%s %s. %s" % (self.menu.slug, self.order, self.title)  
+ 
+    def __unicode__(self):
+        return _("%s %s. %s" % (self.menu.slug, self.order, self.title))
